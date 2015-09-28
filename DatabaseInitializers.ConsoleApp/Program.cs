745ef6b76c4988679b7df2978fee21b41d5b51df
krasimir.triov <krasimir.triov@mentormate.com>
2015-09-28 12:34:39 +0300

    DatabaseInitializers.ConsoleApp/Program.cs tweaks

--- DatabaseInitializers.ConsoleApp/Program.cs
+++ DatabaseInitializers.ConsoleApp/Program.cs
@@ -7,9 +7,14 @@ namespace DatabaseInitializers.ConsoleApp
     {
         static void Main(string[] args)
         {
+            //System.Data.Entity.Database.SetInitializer(new MyInitializer());
             using (var db = new PremierLeagueContext())
-            {
+            {            
                 var players = db.Players.ToList();
+                if(players.Count == 0)
+                {
+                    System.Console.WriteLine("There are no players in the database");
+                }
                 players.ForEach(p => System.Console.WriteLine(p));
             }
         }
